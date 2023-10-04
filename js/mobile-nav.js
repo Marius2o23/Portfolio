20a59f4ad0eada9cdc901527c53594d9c1a83ddb
Marius2o23 <marius.copil91@yahoo.ro>
2023-10-04 18:33:55 +0100

    added mobile dropdown nav

---
+++ js/mobile-nav.js
@@ -0,0 +1,13 @@
+const dropdownButton = document.getElementById('dropdown-button');
+const mobileDropdown = document.querySelector('.mobile-dropdown');
+
+dropdownButton.addEventListener('click', function() {
+    mobileDropdown.classList.toggle('active');
+});
+
+const mobileMenuItems = mobileDropdown.querySelectorAll('li');
+mobileMenuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        mobileDropdown.classList.remove('active');
+    });
+});
